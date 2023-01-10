68b01fc004fe0f7ea58ec2eec3f02b65f57e2a04
Ndekocode <arickbulakali@gmail.com>
2023-01-10 16:03:47 +0200

    setup: add all docs in repos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,69 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btnStart = document.querySelector("#start");
-  /**Will be called on succed si l'utilisateur accepte l'utilisation du micro et de la webcam */
-  const onSuccess = (stream) => {
-    const senderVideo = document.getElementById("sender-video");
-    senderVideo.srcObject = stream;
-    senderVideo.play();
-    console.log(senderVideo.srcObject);
-  };
+document.addEventListener("DOMContentLoaded", function () {
+  /** On annule le comportemen par defaut du formulaire */
+  document.getElementById("incoming").addEventListener("submit", (evt) => {
+    evt.preventDefault();
+  });
 
-  /**Will be called on succed si l'utilisateur refuse l'utilisation du micro et de la webcam */
-  const onError = (error) => {};
-  const params = {
-    video: true,
-    audio: true,
+  // On definit un ensemble d'evenement qui seront detecter et executer lors de la connexion peep to Peer
+  const bindEvent = (peerConnexion) => {
+    document.getElementById("incoming").addEventListener("submit", (evt) => {
+      peerConnexion.signal(JSON.parse(document.getElementById("text").value));
+    });
+    peerConnexion.on("error", (err) => {
+      console.log(err);
+    });
+    peerConnexion.on("signal", (data) => {
+      document.getElementById("offer").value = JSON.stringify(data);
+    });
+    peerConnexion.on("stream", (stream) => {
+      const video = document.getElementById("receiver-video");
+      video.volume = 0;
+      video.srcObject = stream;
+      video.play();
+    });
   };
+  // Le button pour demarrer LA CONVERSION
+  const btnStart = document.getElementById("start");
+  // On fait la compatibilité avec les autres utilisateurs
+  navigator.getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+  /**
+   * La fonction pour initialiser une connexion en peer to peer
+   * @param {Boolean} initiator
+   */
+  const startPeer = (initiator = true) => {
+    const onSuccess = (stream) => {
+      const peerConnexion = new SimplePeer({
+        initiator: initiator,
+        stream: stream,
+        trickle: false,
+      });
+      bindEvent(peerConnexion);
+
+      const videoEmitter = document.getElementById("emitter-video");
+      videoEmitter.volume = 0;
+      videoEmitter.srcObject = stream;
+      videoEmitter.play();
+    };
+    navigator.getUserMedia(
+      {
+        video: true,
+        audio: true,
+      },
+      onSuccess,
+      () => {}
+    );
+  };
+  /** Le boutton pour demarrer la communication */
   btnStart.addEventListener("click", () => {
-    navigator.getUserMedia(params, onSuccess, onError);
+    startPeer(true);
+  });
+  /** Le boutton pour receivoir la communication */
+  const btnReceiver = document.getElementById("receive");
+  btnReceiver.addEventListener("click", () => {
+    startPeer(false);
   });
 });
